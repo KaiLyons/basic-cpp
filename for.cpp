1ecffbff5f1e5bf2513dbe5ed28f6230f1133c3c
MoisturePackets <lyons.safyre@gmail.com>
2019-08-02 17:12:23 -0600

    Create for.cpp

---
+++ for.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    for(int i = 1; i <= /* does it as many times as you set there ----> */ 10; i++ ){
+        cout << "For loop" << endl;
+    }
+
+    /*
+        How it works:
+        - checks if i = 1
+        - checks if less than or equal to ten (or any number you set)
+        - does it 10 times (or does it as many times as the num you set)
+    */
+}
